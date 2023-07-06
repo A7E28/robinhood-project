fe4e05dc4d5928c947995bf6212f57c3b2b828cf
Mr. Unicorn <48880125+mrun1corn@users.noreply.github.com>
2023-07-06 10:25:03 +0600

    re-organize code and add funtion for checking different status in different time

--- ping_bot.py
+++ ping_bot.py
@@ -5,22 +5,25 @@ import subprocess
 
 # Telegram API credentials
 API_ID = app_id
-API_HASH = 'api_has'
+API_HASH = 'api_hash'
 BOT_TOKEN = 'bot_token'
-
-# Chat ID where the updates will be sent
 CHAT_ID = chat_id
 
 # IP addresses and their corresponding names
 IP_NAME_MAPPING = {
     "8.8.8.8": "Google DNS",
-    "192.168.1.1": "Router",
-    "192.168.1.104": "Mobile",
+    "192.168.0.1": "Router",
+    "192.168.0.101": "Mobile",
 }
 
 # Initialize the Telegram client
 client = TelegramClient('status_bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
+# Dictionary to store the online status of devices
+online_status = {}
+
+# Dictionary to store the offline status of devices
+offline_status = {}
 
 async def check_status(ip):
     try:
@@ -35,21 +38,34 @@ async def check_status(ip):
         return None
 
 
-async def send_status():
+async def send_online_status():
     online_devices = []
-    offline_devices = []
     
     for ip, name in IP_NAME_MAPPING.items():
         response_time = await check_status(ip)
         
         if response_time is not None:
             online_devices.append(f"{name} ({ip}) - Response Time: {response_time} ms")
-        else:
-            offline_devices.append(f"{name} ({ip})")
+            online_status[ip] = response_time
+            # Remove from offline_status if previously offline
+            offline_status.pop(ip, None)
     
     if online_devices:
         online_message = "Online Devices:\n\n" + "\n".join(online_devices)
         await client.send_message(CHAT_ID, online_message)
+
+
+async def send_offline_status():
+    offline_devices = []
+    
+    for ip, name in IP_NAME_MAPPING.items():
+        response_time = await check_status(ip)
+        
+        if response_time is None:
+            offline_devices.append(f"{name} ({ip})")
+            offline_status[ip] = True
+            # Remove from online_status if previously online
+            online_status.pop(ip, None)
     
     if offline_devices:
         offline_message = "Offline Devices:\n\n" + "\n".join(offline_devices)
@@ -58,7 +74,8 @@ async def send_status():
 
 @client.on(events.NewMessage(pattern='/status'))
 async def get_status(event):
-    await send_status()
+    await send_online_status()
+    await send_offline_status()
 
 
 async def run_schedule():
@@ -67,8 +84,11 @@ async def run_schedule():
         await asyncio.sleep(1)
 
 
-# Schedule the status update every 5 seconds
-schedule.every(5).seconds.do(lambda: asyncio.create_task(send_status()))
+# Schedule sending online status every 10 seconds
+schedule.every(10).seconds.do(lambda: asyncio.create_task(send_online_status()))
+
+# Schedule sending offline status every 5 seconds
+schedule.every(5).seconds.do(lambda: asyncio.create_task(send_offline_status()))
 
 # Run the client
 async def main():
