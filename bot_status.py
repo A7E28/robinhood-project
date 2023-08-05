a46e6c3c7bc8929b72f5b182d03e5a9e434f00f8
mrun1corn <robinisalegend01@gmail.com>
2023-08-05 22:32:19 +0600

    added visualized monitoring

--- bot_status.py
+++ bot_status.py
@@ -1,9 +1,13 @@
-# bot_status.py
-
 import psutil
 import time
 from telethon import events
 from credentials import API_ID, API_HASH, BOT_TOKEN
+
+# Function to generate a text-based progress bar
+def progress_bar(percentage):
+    filled_blocks = int(round(percentage / 10))
+    empty_blocks = 10 - filled_blocks
+    return "▓" * filled_blocks + "░" * empty_blocks
 
 def register_bot_status_feature(client, start_time):
     @client.on(events.NewMessage(pattern='/bot_status'))
@@ -21,9 +25,21 @@ def register_bot_status_feature(client, start_time):
         message = await event.respond("Calculating ping...")
         end_ping = time.time()
         ping_time = (end_ping - start_ping) * 1000
-        await message.edit(f"Bot Uptime: {uptime_str}\n"
-                           f"CPU Usage: {cpu_percent}%\n"
-                           f"RAM Usage: {ram_usage}%\n"
-                           f"Ping: {ping_time:.2f} ms")
+
+        # Create graphical representations for CPU and RAM usage
+        cpu_progress = progress_bar(cpu_percent)
+        ram_progress = progress_bar(ram_usage)
+
+        # Compose the response with the graphical representations
+        response = (
+            f"Bot Uptime: {uptime_str}\n"
+            f"CPU Usage: {cpu_percent}%\n"
+            f"{cpu_progress} {cpu_percent}%\n"
+            f"RAM Usage: {ram_usage}%\n"
+            f"{ram_progress} {ram_usage}%\n"
+            f"Ping: {ping_time:.2f} ms"
+        )
+
+        await message.edit(response)
 
         raise events.StopPropagation
