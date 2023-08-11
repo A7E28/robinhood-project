80ee3a9c8e4d5a674340b6a7a03078d4e09ad59d
Mr. Unicorn <48880125+mrun1corn@users.noreply.github.com>
2023-08-11 13:01:06 +0600

    Update help.py

--- help.py
+++ help.py
@@ -8,11 +8,11 @@ def register_help_feature(client):
             "List of available commands:\n"
             "/start - Start the bot\n"
             "/help - Display this help message\n"
-            "/time - Show current date and time"
-            "/bot_status - Show the information of server"
-            "/all_status - this will check and send status of all the device from device.txt"
-            "/status_device(device name or ip) - this will provide the mentioned device in the command"
-	    "/subnet_calculator - this is a subetmask and cidr convert calculator"
+            "/time - Show current date and time\n"
+            "/bot_status - Show the information of server\n"
+            "/all_status - this will check and send status of all the device from device.txt\n"
+            "/status_device(device name or ip) - this will provide the mentioned device in the command\n"
+	    "/subnet_calculator - this is a subetmask and cidr convert calculator\n"
         )
         await event.respond(help_text)
         raise events.StopPropagation
