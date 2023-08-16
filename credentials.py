8d00f252d01597f62206f06f8b874557f5a4a119
mrun1corn <robinisalegend01@gmail.com>
2023-08-16 21:32:59 +0600

    move api and url to the credentials

--- credentials.py
+++ credentials.py
@@ -5,3 +5,5 @@ API_ID = 27024327
 API_HASH = '669bdeddb70a2961aafcad641528aead'
 BOT_TOKEN = '6233443371:AAEMU3svmTajA0wnLEKjQHa4cXmmbwtfFHY'
 CHAT_ID = 1436979843
+API_KEY = '64cd31157c219d9a000e22b9d9780960'
+API_URL = 'https://api.openweathermap.org/data/2.5/weather'
