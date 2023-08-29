f30a881560983540d7f3a2f822bf4d61a7ed36e3
mrun1corn <robinisalegend01@gmail.com>
2023-08-29 20:06:34 +0600

    re-workng on status.py

--- status.py
+++ status.py
@@ -1,7 +1,12 @@
+import asyncio
 import ping3
 from telethon import events
 import re
 import json
+import time
+
+# Define the interval (in seconds) for continuous status checks
+CHECK_INTERVAL = 5  # 5 minutes
 
 def get_response_time(ip):
     try:
@@ -28,6 +33,17 @@ def get_device_name_and_ip(identifier):
 def save_devices_to_json(devices):
     with open('device.json', 'w') as file:
         json.dump({"devices": devices}, file, indent=4)
+
+# Load or create status.json
+try:
+    with open('status.json', 'r') as status_file:
+        status_data = json.load(status_file)
+except FileNotFoundError:
+    status_data = {}
+
+# Store previous status in status_data
+for ip in status_data:
+    status_data[ip]['prev_status'] = status_data[ip]['status']
 
 def register_status_feature(client):
     @client.on(events.NewMessage(pattern='/status_device'))
@@ -119,3 +135,65 @@ def register_status_feature(client):
             device_list_msg += f"{idx}. {name} ({ip})\n"
 
         await event.respond(device_list_msg)
+        
+    # Function to send initial status updates for all devices
+    async def send_initial_status_updates(event):
+        with open('device.json', 'r') as device_file:
+            devices = json.load(device_file)
+
+        for device in devices['devices']:
+            ip = device['ip']
+            response_time, is_online = get_response_time(ip)
+
+            if is_online:
+                status_msg = f"{device['name']} is online: {response_time}"
+            else:
+                status_msg = f"{device['name']} is offline"
+
+            await event.respond(status_msg)
+
+    # Function to check the status of all devices
+    async def continuous_status_check(event):
+        with open('device.json', 'r') as device_file:
+            devices = json.load(device_file)
+
+        initial_status_sent = False  # Flag to track if initial status updates have been sent
+
+        while True:
+            print("Checking device statuses...")
+            for device in devices['devices']:
+                ip = device['ip']
+                response_time, is_online = get_response_time(ip)
+
+                if ip in status_data:
+                    if is_online != (status_data[ip]['status'] == 'online'):
+                        status_data[ip]['status'] = 'online' if is_online else 'offline'
+                        status_data[ip]['timestamp'] = time.time()
+
+                        status_msg = f"{device['name']} is now {status_data[ip]['status']}"
+                        await event.respond(status_msg)  # Use event.respond() to send the status change notification
+
+                else:
+                    status_data[ip] = {'status': 'online' if is_online else 'offline', 'prev_status': None, 'timestamp': time.time()}
+
+                if not initial_status_sent:
+                    if is_online:
+                        initial_status_msg = f"{device['name']} is online: {response_time}"
+                    else:
+                        initial_status_msg = f"{device['name']} is offline"
+
+                    await event.respond(initial_status_msg)
+            
+            initial_status_sent = True  # Set the flag to True after sending initial status updates
+
+            # Save the status to status.json
+            with open('status.json', 'w') as status_file:
+                json.dump(status_data, status_file, indent=4)
+
+            await asyncio.sleep(CHECK_INTERVAL)
+
+    # Register the continuous_status_check function
+    @client.on(events.NewMessage(pattern='/continuous_check'))
+    async def start_continuous(event):
+        await event.respond("Continuous status checks started.")
+        await continuous_status_check(event)  # Pass the event to the function
