5467fd4f7affeeb7f64a562a754002dd721ba9ae
mrun1corn <robinisalegend01@gmail.com>
2023-10-10 10:58:47 +0600

    added ip_info help

--- help.py
+++ help.py
@@ -10,9 +10,10 @@ def register_help_feature(client):
             "/help - Display this help message\n"
             "/time - Show current date and time\n"
             "/bot_status - Show the information of server\n"
-	    "/subnet_calculator - this is a subetmask and cidr convert calculator\n"
-	    "/setlocation (your desired location) - set your location to get wather information on /weather command\n"
-	    "/weather - for weather information of your given location\n"
+            "/subnet_calculator - this is a subetmask and cidr convert calculator\n"
+            "/setlocation (your desired location) - set your location to get wather information on /weather command\n"
+            "/weather - for weather information of your given location\n"
+            "/ip_info - for getting information of a ip or domain\n"
         )
         await event.respond(help_text)
         raise events.StopPropagation
